3ee79830aa7cf00235d9170efd569de2c76c81fb
pablorodriper <parope2@gmail.com>
2018-10-14 20:51:23 +0200

    Only find .jpg

--- traffic_signs/main.py
+++ traffic_signs/main.py
@@ -6,10 +6,12 @@ import os
 import numpy as np
 import shutil
 import time
+import glob
 
 from evaluation.evaluation_funcs import performance_evaluation_pixel
 from utils import get_img, rgb2hsv, threshold_image, save_image, get_files_from_dir, confusion_matrix, \
     print_confusion_matrix, print_metrics
+
 
 # Logger setup
 logging.basicConfig(
@@ -21,14 +23,20 @@ logger = logging.getLogger(__name__)
 
 
 # Directory in the root directory where the results will be saved
-RESULT_DIR = os.path.join('results')
 # Useful directories
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 TRAIN_DIR = os.path.join('dataset', 'train')
 TRAIN_GTS_DIR = os.path.join(TRAIN_DIR, 'gt')
 TRAIN_MASKS_DIR = os.path.join(TRAIN_DIR, 'mask')
-TEST_DIR = os.path.join('dataset', 'test')
+#TEST_DIR = os.path.join('dataset', 'test')
 
+train = False
+if train:
+    RESULT_DIR = os.path.join('results', 'train')
+    IMAGE_DIR = TRAIN_DIR
+else:
+    RESULT_DIR = os.path.join('results', 'test')
+    IMAGE_DIR = os.path.join('dataset', 'test')
 
 # If the directory already exists, delete it
 if os.path.exists(RESULT_DIR):
@@ -38,8 +46,8 @@ if os.path.exists(RESULT_DIR):
 os.makedirs(RESULT_DIR)
 
 # Get list of test images in test directory
-# test_images = get_files_from_dir(TRAIN_DIR)
-test_images = os.listdir(TEST_DIR)
+test_images = get_files_from_dir(IMAGE_DIR)
+#test_images = os.listdir(TEST_DIR)
 
 # Set threshold based on ranges of interest
 ths_h = np.array([
@@ -55,10 +63,12 @@ t0 = time.time()
 t_frame = 0
 
 # Iterate over test image paths
-for img_dir in test_images:
+for img_dir_temp in glob.glob(IMAGE_DIR + "/*.jpg"):
+    img_dir = img_dir_temp.split("/")[-1]
+
     t_frame_0 = time.time()
     # Get numpy array of the image and convert it to HSV
-    img = get_img(TEST_DIR, img_dir)
+    img = get_img(IMAGE_DIR, img_dir)
     img_hsv = rgb2hsv(img)
     # img_hsv = ndimage.filters.gaussian_filter(img_hsv, sigma=3)
 
@@ -68,7 +78,7 @@ for img_dir in test_images:
     #mask += threshold_image(img_hsv, ths_v, channel=2)
 
     # Create a numpy array where mask values are 255
-    final = mask.astype(np.uint8)
+    final = mask.astype(np.uint8)*255
 
     # Save mask as image
     fn, d = save_image(final, RESULT_DIR, img_dir)
